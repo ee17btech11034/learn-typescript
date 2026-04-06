bc6347ceb2a208f2b8ad20aa57e6e55e65a05b28
ee17btech11034 <asiwalraja@gmail.com>
2026-04-06 08:53:58 +0530

    OOP, access modifiers private public protected, static keyword, abstract class & methods, Composition vs inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,136 +1,87 @@
-// #################      Array
+// ######### OOP
 
-const chaiFlavours: string[] = ["str1", "str2"] // all elements must be of string
-const chaiFlavours2: number[] = [1, 2] // all elements must be of number
-
-const rating: Array<number> = [4,5,6] // we can use custom datat types instead of number as well 
-
-
-
-// arr of objects
-
-type Chai = {
-    name: string;
-    price: number 
-}
-
-const menu: Chai[] = [
-    {
-        name: "Masala",
-        price: 20
-    },
-    {
-        name: "Ginger",
-        price: 25
-    }
-]
-
-
-
-// readonly arr 
-
-const cities: readonly string[] = ["Delhi", "Jaipur"]
-
-// cities.push("Pune") // error as we can not edit this arr
-
-
-// sometimes we do unions and all
-
-
-// multidimentional arr
-const table: number[][] = [
-    [1,2,3],
-    [4,5,6]
-]
-
-
-
-
-
-// ########## tuples
-
-let chaiTuple: [string, number];
-
-// chaiTuple = [20, "Masala"] // error as exact format is needed 
-chaiTuple = ["Masala", 20] 
-
-let userInfo: [string, number, boolean?] // here boolean is optional
-userInfo = ["masala", 20] // no error here
-userInfo = ["masala", 20, true] // no error here
-
-
-// readonly tuple
-// we define readonly property first in all scenarios
-
-const location: readonly [number, number] = [26.7, 34.7]
-
-
-// named tuples
-const chaiIteams: [name: string, price: number] = ["abc", 20]
-
-
-// push working in tuple
-let t: [string] = ["Hello"]
-t.push("extra") // this will work. It can cause anomalies some times
-
-
-
-
-// #######################   Enums 
-// restrict the choices
-
-enum CupSize {
-    SMALL,
-    MEDIUM,
-    LARGE
-}
-
-const size = CupSize.LARGE
-
-
-
-// incremental values 
 /*
-enum status {
-    PENDING = 100,
-    SERVED, // 101  // if we do not give the value to it thn they get incremental val from above one
-    CANCELLED // 102
+class Chai {
+    flavour: string;
+    price: number // it is throwing warning as it is asking for constructor
 }
-    */
-enum status {
-    PENDING = 100,
-    SERVED, //101
-    CANCELLED //102
-}
+*/
 
+class Chai {
+    flavour: string;
+    price: number
 
-
-// 
-enum ChaiType {
-    MASALA = "masala",
-    GINGER = "ginger"
-}
-function makeChai(type: ChaiType){
-    console.log(type);
-}
-makeChai(ChaiType.GINGER) // valid 
-// makeChai("masala") // error as we can only select from those values
-
-
-// Haterogenous values
-// multiple types in a single enum
-// Not a good practice but 
-
-enum RandomEnum {
-    ID = 1,
-    NAME= "Raj"
+    constructor(flavour: string, price: number){
+        this.flavour = flavour;
+        this.price = price
+    }
 }
 
-// const keyword in enum
-const enum CupSizes {
-    SMALL = 100,
-    MEDIUM = 300,
-    LARGE = 500
+const masalaChai = new Chai("Ginger", 20)
+masalaChai.flavour = "Masala"
+
+
+
+
+// Access Modifiers ==> public, protected, private
+class CHai2 {
+    public flavour: string = "Masala"
+
+    private secret = 'abc' // can not access directly outside class
+    #price = 500 // this is also private (it comes from JS)
+
+    readonly capacity: number = 200 // can set during constructor/only once
+
+
+    // can write getter setter as well
+    private _val1 = 2
+    get val1(){
+        return this._val1
+    }
+    set val1(val: number){
+        this._val1 = val
+    }
+
+
+    // static member
+    static shopName = "MB"
+    constructor(public flavour2: string[]){
+
+    }
 }
 
-const s = CupSizes.LARGE
+// static can only be accessed by        "CHai2.shopName"    not from instance of class. 
+
+
+
+// abstract class:-> we restrict that no one can create object from these
+abstract class Drink {
+    abstract make(): void // mostly only syntax are defined in abstract classes
+}
+
+// we have to create a class to use this
+
+// class MyChai extends Drink{} // error untill you define make method
+
+class MyChai extends Drink{
+    make(): void {
+        console.log("hahaaha");
+    }
+}
+
+
+
+
+// composition --> 
+// sometimes people composition, somtimes inheritance
+class Heater{
+    heat(){}
+}
+class ChaiMaker{
+    constructor(private heater: Heater){
+
+    }
+    make(){
+        this.heater.heat
+    }
+}
